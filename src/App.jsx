84a0fcd14c771132157ae6a8e72e391ca1317953
Mourad beddibi <beddibimourad@gmail.com>
2024-03-24 18:33:15 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Counter from "./Counter";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-dvh border bg-indigo-950">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
